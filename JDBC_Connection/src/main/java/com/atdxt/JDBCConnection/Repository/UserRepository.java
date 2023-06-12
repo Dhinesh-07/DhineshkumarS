be330df26359b138bb1560ba74531cea525d208e
Dhinesh-07 <dhineshkumar4567@gmail.com>
2023-06-12 13:49:36 +0530

    Update .gitignore to exclude mvnfile directory

--- JDBC_Connection/src/main/java/com/atdxt/JDBCConnection/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atdxt.JDBCConnection.Repository;
-
-import com.atdxt.JDBCConnection.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
