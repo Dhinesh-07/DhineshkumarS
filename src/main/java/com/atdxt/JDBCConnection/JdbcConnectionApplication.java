5f62c984acd077a32af1f79ca5d6dbc2f629b129
Dhinesh-07 <dhineshkumar4567@gmail.com>
2023-06-13 14:53:08 +0530

    logging complted

--- src/main/java/com/atdxt/JDBCConnection/JdbcConnectionApplication.java
+++ src/main/java/com/atdxt/JDBCConnection/JdbcConnectionApplication.java
@@ -1,21 +1,27 @@
 package com.atdxt.JDBCConnection;
 
 
+import com.atdxt.JDBCConnection.Controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JdbcConnectionApplication {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	public static void main(String[] args) {
 		try {
-			// Intentionally causing an error by accessing an invalid index
+			logger.info("all correct staement");
+
 			int[] array = {1, 2, 3};
 			int value = array[5];
-		} catch (ArrayIndexOutOfBoundsException e) {
-			// Handling the error
+		}  catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("An error occurred: " + e.getMessage());
+			logger.error("error bound",e);
 		}
+
 		SpringApplication.run(JdbcConnectionApplication.class, args);
 	}
 
